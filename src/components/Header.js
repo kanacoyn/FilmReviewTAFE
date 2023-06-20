d60efc2ebeff15ca7393a606a71818d5c271b669
Kanako <kanakoyano.code@gmail.com>
2023-06-20 12:43:40 +1000

    Redirect to home page with Icon click

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,24 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import { Navigation } from "./Navigation";
+import movieIcon from "../movieIcon.png";
 
 export function Header(props) {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand>Film Stack OverView</Navbar.Brand>
+        <Navbar.Brand>
+          <a href="/" className="home-link">
+            <img
+              src={movieIcon}
+              alt="Movie Icon"
+              width="40"
+              height="40"
+              className="movie-icon"
+            />
+          </a>
+          Film Stack OverView
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-nav" />
         <NavbarCollapse id="main-nav">
           <Navigation />
